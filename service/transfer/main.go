71a76e3a362594a4deb777adc5f5a73e1aca300e
CrowFea <wendaoliu40@outlook.com>
2020-04-10 15:41:11 +0800

    change the main func

--- service/transfer/main.go
+++ service/transfer/main.go
@@ -1,1 +1,55 @@
-package transfer
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"Alfred/config"
+	dblayer "Alfred/db"
+	"Alfred/mq"
+	"Alfred/store/oss"
+	"log"
+	"os"
+)
+
+// ProcessTransfer : 处理文件转移
+func ProcessTransfer(msg []byte) bool {
+	log.Println(string(msg))
+	//解析msg
+	pubData := mq.TransferData{}
+	err := json.Unmarshal(msg, &pubData)
+	if err != nil {
+		log.Println(err.Error())
+		return false
+	}
+	//找到文件临时存储路径
+	fin, err := os.Open(pubData.CurLocation)
+	if err != nil {
+		log.Println(err.Error())
+		return false
+	}
+	//写入oss
+	err = oss.Bucket().PutObject(
+		pubData.DestLocation,
+		bufio.NewReader(fin))
+	if err != nil {
+		log.Println(err.Error())
+		return false
+	}
+	//更新文件表信息
+	_ = dblayer.UpdateFileLocation(
+		pubData.FileHash,
+		pubData.DestLocation)
+	return true
+}
+
+func main() {
+	if !config.AsyncTransferEnable {
+		log.Println("异步转移文件功能目前被禁用，请检查相关配置")
+		return
+	}
+	log.Println("文件转移服务启动中，开始监听转移任务队列...")
+	mq.StartConsume(
+		config.TransOSSQueueName,
+		"transfer_oss",
+		ProcessTransfer)
+}
