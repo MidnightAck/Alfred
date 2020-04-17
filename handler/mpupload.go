8d2c7584dfc26d2f02556e4e7ed3c487fe56e72f
CrowFea <wendaoliu40@outlook.com>
2020-04-17 09:40:39 +0800

    change multi

--- handler/mpupload.go
+++ handler/mpupload.go
@@ -4,11 +4,17 @@ import (
 	rPool "Alfred/cache/redis"
 	dblayer "Alfred/db"
 	"Alfred/util"
+	"bufio"
+	"bytes"
 	"cloudstore/config"
 	"fmt"
 	"github.com/garyburd/redigo/redis"
+	jsonit "github.com/json-iterator"
+	"io"
+	"io/ioutil"
 	"math"
 	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
 	"path"
@@ -169,4 +175,124 @@ func mergeAllPartFile(partFileStorePath, fileStorePath string) (bool, error) {
 	}
 	fmt.Println(fileStorePath, " has been merge complete")
 	return true, nil
+}
+
+func multipartUpload(filename string, targetURL string, chunkSize int) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer f.Close()
+
+	bfRd := bufio.NewReader(f)
+	index := 0
+
+	ch := make(chan int)
+	buf := make([]byte, chunkSize) //每次读取chunkSize大小的内容
+	for {
+		n, err := bfRd.Read(buf)
+		if n <= 0 {
+			break
+		}
+		index++
+
+		bufCopied := make([]byte, 5*1048576)
+		copy(bufCopied, buf)
+
+		go func(b []byte, curIdx int) {
+			fmt.Printf("upload_size: %d\n", len(b))
+
+			resp, err := http.Post(
+				targetURL+"&index="+strconv.Itoa(curIdx),
+				"multipart/form-data",
+				bytes.NewReader(b))
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			body, er := ioutil.ReadAll(resp.Body)
+			fmt.Printf("%+v %+v\n", string(body), er)
+			resp.Body.Close()
+
+			ch <- curIdx
+		}(bufCopied[:n], index)
+
+		//遇到任何错误立即返回，并忽略 EOF 错误信息
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println(err.Error())
+			}
+		}
+	}
+
+	for idx := 0; idx < index; idx++ {
+		select {
+		case res := <-ch:
+			fmt.Println(res)
+		}
+	}
+
+	return nil
+}
+
+func DoMpupload(username string,token string,filehash string,filesize string,filename string) {
+	// 1. 请求初始化分块上传接口
+	resp, err := http.PostForm(
+		"http://localhost:8080/file/mpupload/init",
+		url.Values{
+			"username": {username},
+			"token":    {token},
+			"filehash": {filehash},
+			"filesize": {filesize},
+		})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
+
+	// 2. 得到uploadID以及服务端指定的分块大小chunkSize
+	uploadID := jsonit.Get(body, "data").Get("UploadID").ToString()
+	chunkSize := jsonit.Get(body, "data").Get("ChunkSize").ToInt()
+	//fmt.Printf("uploadid: %s  chunksize: %d\n", uploadID, chunkSize)
+
+	// 3. 请求分块上传接口
+	tURL := "http://localhost:8080/file/mpupload/uppart?" +
+		"username=admin&token=" + token + "&uploadid=" + uploadID
+	multipartUpload(filename, tURL, chunkSize)
+
+	// 4. 请求分块完成接口
+	resp, err = http.PostForm(
+		"http://localhost:8080/file/mpupload/complete",
+		url.Values{
+			"username": {username},
+			"token":    {token},
+			"filehash": {filehash},
+			"filesize": {filesize},
+			"filename": {filename},
+			"uploadid": {uploadID},
+		})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
+
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
+	fmt.Printf("complete result: %s\n", string(body))
 }
